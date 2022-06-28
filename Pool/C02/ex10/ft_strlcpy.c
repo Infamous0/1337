e9c2a4dad2504d36434dbbdcd695e32a70d8871a
Infamous0 <72175468+Infamous0@users.noreply.github.com>
2022-06-28 20:29:08 +0100

    Add files via upload

---
+++ Pool/C02/ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amouhoub <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 20:49:19 by amouhoub          #+#    #+#             */
+/*   Updated: 2022/06/20 20:57:43 by amouhoub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	res;
+
+	i = 0;
+	res = 0;
+	while (src[res])
+		res++;
+	if (size < 1)
+		return (res);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (res);
+}
